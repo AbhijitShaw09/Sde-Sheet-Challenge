bbddd52b376d9b712f39c06f11e9c5792a569170
AbhijitShaw09 <78562439+AbhijitShaw09@users.noreply.github.com>
2022-06-12 10:50:31 +0530

    Added code of Minimum Number of Platforms

---
+++ minimumNumberOfPlatforms.cpp
@@ -0,0 +1,19 @@
+int calculateMinPatforms(int at[], int dt[], int n) {
+    // Write your code here.
+    sort(at, at+n);
+    sort(dt, dt+n);
+
+    int ans = 1, count = 1, i = 1, j = 0;
+    while(i < n && j < n) {
+        if(at[i] <= dt[j]) { //one more platform needed
+            count++;
+            i++;
+        }
+        else { //one platform can be reduced
+            count--;
+            j++;
+        }
+        ans = max(ans, count); //updating the value with the current maximum
+    }
+    return ans;
+}
