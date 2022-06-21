65010ed5d630d6e9c98def2b100b634bac95ff84
AbhijitShaw09 <78562439+AbhijitShaw09@users.noreply.github.com>
2022-06-21 10:39:40 +0530

    Added code of Search In Rotated Sorted Array

---
+++ searchInRotatedSortedArray.cpp
@@ -0,0 +1,23 @@
+int search(int* nums, int n, int target) {
+    // Write your code here.
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) >> 1;
+        if (nums[mid] == target)
+            return mid;
+
+        if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && nums[mid] >= target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        } else {
+            if (nums[mid] <= target && target <= nums[high])
+            low = mid + 1;
+            else
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
