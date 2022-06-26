76a292bcc73834c614257b92f90ada8a850855a4
AbhijitShaw09 <78562439+AbhijitShaw09@users.noreply.github.com>
2022-06-26 10:30:09 +0530

    Added code of Stack Using Queue

---
+++ stackUsingQueue.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+class Stack {
+	// Define the data members.
+    int siz;
+    queue<int> q;
+   public:
+    Stack() {
+        // Implement the Constructor.
+        siz = -1;
+    }
+
+    /*----------------- Public Functions of Stack -----------------*/
+
+    int getSize() {
+        // Implement the getSize() function.
+        return q.size();
+    }
+
+    bool isEmpty() {
+        // Implement the isEmpty() function.
+        return (q.empty());
+    }
+
+    void push(int element) {
+        // Implement the push() function.
+        siz++;
+        q.push(element);
+        for(int i=0;i<siz;i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        // Implement the pop() function.
+        if(!q.empty()){
+            int popped = q.front();
+            q.pop();
+            siz--;
+            return popped;
+        }
+        return -1;
+    }
+
+    int top() {
+        // Implement the top() function.
+        if(!q.empty())
+           return q.front();
+       return -1;
+    }
+};
