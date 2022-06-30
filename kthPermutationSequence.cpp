33c299bf2ed93adf8f494ce8f0e9019d1738d8f3
AbhijitShaw09 <78562439+AbhijitShaw09@users.noreply.github.com>
2022-06-30 12:20:15 +0530

    Added code of K-th Permutation Sequence

--- kthPermutationSequence.cpp
+++ kthPermutationSequence.cpp
@@ -1,25 +1,25 @@
 #include<bits/stdc++.h>
-void permuteString(string &s, int idx, vector<string> &ans)
-{
-    if(idx >= s.size()) {
-        ans.push_back(s);
-        return;
-    }
-    for(int i=idx; i<s.size(); i++) {
-        swap(s[i], s[idx]);
-        permuteString(s, idx+1, ans);
-        swap(s[i], s[idx]);
-    }
-    return;
-}
 
 string kthPermutation(int n, int k) {
     // Write your code here.
-    vector<string> ans;
-    string s = "";
-    for(char i='1'; i<'1'+n; i++) 
-        s += i;
-    permuteString(s, 0, ans);
-    sort(ans.begin(), ans.end());
-    return ans[k-1];
+    vector<int>arr;
+    string ans = "";
+    int fact = 1;
+    
+    for(int i=1; i<n; i++) {
+        fact *= i;
+        arr.push_back(i);
+    }
+    arr.push_back(n);
+    k -= 1;
+    
+    while(true) {
+        ans += to_string(arr[k/fact]);
+        arr.erase(arr.begin()+k / fact);
+        if(arr.size() == 0)
+            break;
+        k = k % fact;
+        fact = fact / arr.size();
+    }
+    return ans;
 }
