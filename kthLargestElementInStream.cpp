51ebaa69577c11e7f1d37dd81d967bf7cad8c806
AbhijitShaw09 <78562439+AbhijitShaw09@users.noreply.github.com>
2022-07-16 07:01:41 +0530

    Added code of Kth Largest Element In A Stream

---
+++ kthLargestElementInStream.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+class Kthlargest {
+    priority_queue<int,vector<int>,greater<int>>q;
+public:
+    Kthlargest(int k, vector<int> &arr) {
+       // Write your code here.
+        for (int i=0;i<arr.size();i++)
+           q.push(arr[i]);
+    }
+
+    void add(int num) {
+        // Write your code here.
+        if (num <= q.top())
+            return;
+        q.push(num);
+        q.pop();
+    }
+
+    int getKthLargest() {
+       // Write your code here.
+        return q.top();
+    }
+
+};
