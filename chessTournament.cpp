ee88754fdd72b3d741cbf209af3a6d4657083b20
AbhijitShaw09 <78562439+AbhijitShaw09@users.noreply.github.com>
2022-07-17 13:24:53 +0530

    Added code of Chess Tournament

---
+++ chessTournament.cpp
@@ -0,0 +1,30 @@
+bool canPlace(int dist, int c, vector<int> positions, int n) {
+    int ninjasplaced = 1;
+    int recentplacedat = 0;
+    for(int i=1; i<n; i++) {
+        if(positions[i]-positions[recentplacedat] >= dist) {
+            ninjasplaced++;
+            recentplacedat = i;
+        }
+    }
+    if(ninjasplaced >= c)
+        return true;
+    else
+        return false;
+}
+
+int chessTournament(vector<int> positions , int n ,  int c){
+	// Write your code here
+    sort(positions.begin(),positions.end());
+    int l = 0, ans = 0, h = positions[n-1]-positions[0];
+
+    while(l <= h) {
+        int mid = (l+h)/2;
+        if(canPlace(mid, c, positions, n)) {
+            ans = mid;
+            l = mid+1;
+        } else
+            h = mid-1;
+    }
+    return ans;
+}
